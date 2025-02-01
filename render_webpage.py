ee713f40af7dd3f624654ccd26e2163259be1c1d
Meg Zhang <mzhan562@uwo.ca>
2025-02-01 01:30:25 -0500

    Update render_webpage.py
    Added more presets. Apparently numbers are really broken on this tool. I'll fix it later.

--- render_webpage.py
+++ render_webpage.py
@@ -56,13 +56,23 @@ content = '''
                 <textarea id="text" name="text" rows="4" class="form-control" placeholder="Enter Chinese text here...">{{ request.form['text'] if request.form['text'] else '' }}</textarea>
             </div>
             <!-- Preset Buttons -->
-                <label for="text">Presets: </label>
+                <div class = "button-container">
+                <label for="text">Common Chinese Expressions: </label> <br>
                 <button type="button" class="btn btn-secondary" onclick="setPreset('你好')">Hello</button>
                 <button type="button" class="btn btn-secondary" onclick="setPreset('再见')">Goodbye</button>
                 <button type="button" class="btn btn-secondary" onclick="setPreset('谢谢')">Thank you</button>
+                <button type="button" class="btn btn-secondary" onclick="setPreset('早上好')">Good morning</button>
+                <button type="button" class="btn btn-secondary" onclick="setPreset('晚上好')">Good evening</button>
+                <button type="button" class="btn btn-secondary" onclick="setPreset('晚安')">Good night</button>
+                <button type="button" class="btn btn-secondary" onclick="setPreset('新年快乐')">Happy new year</button>
+                </div>
+                <br>
+                <div class = "button-container">
+                <label for="text">Cool Presets: </label> <br>
                 <button type="button" class="btn btn-secondary" onclick="setPreset('清真')">Halal ¹</button>
                 <button type="button" class="btn btn-secondary" onclick="setPreset('《天方性理》题记: 言心性，无异于儒家。言四元及天有九重，合于欧罗巴之法。盖精研于程朱之理，又纬以泰西之学，遂能卓然成一家之言，为天方教中巨作。明未文体多诡，言理者尤多支碎。此书文笔昌明博大。盖康熙间，景运方隆，文明焕发，而载笔之士，皆知圣道所归，想见一时儒学之盛焉。时同治丙寅岁仲春月。兵部侍郎兼都察院右副都御史、安徽巡抚部院兼提督军门乔松年阅毕题记。 ')">Liu Zhi's Tianfang xingli ²</button>
-
+                </div>
+                
             <button type="submit" class="btn btn-primary btn-block" id="submitButton" style="margin-top: 10px;">Convert</button>
         </form>
         
